e89265303f616ff4dc45c68a747fa98729e6f010
桂子 <guissy@qq.com>
2019-09-02 19:54:09 +0800

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, Suspense } from 'react';
+import pageText1 from "./assets/images/pageText1.png";
+import pageText2 from "./assets/images/pageText2.png";
+import freeBtn from "./assets/images/free-btn.png";
+import arrow from "./assets/images/arrow.png";
+import page2 from "./assets/images/page2.jpg";
+import page3 from "./assets/images/page3.jpg";
 import './App.css';
+const Apply = React.lazy(() => import("./Apply"));
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface StoreEntity {
+  isForm: boolean;
+  toForm?: () => void;
 }
+
+const Store = React.createContext(null as unknown as StoreEntity);
+
+const Main: React.FC<{ toForm: () => void }> = ({ toForm }) => {
+  return <>
+    <div className="banner-box bannerBox">
+      <div className="pageText1">
+        <img src={pageText1}/>
+      </div>
+      <div className="pageText2">
+        <img src={pageText2}/>
+      </div>
+      <a href="#form" onClick={toForm} className="free-btn free-btn1">
+        <img src={freeBtn}/>
+      </a>
+      <div className="arrow-box">
+        <img src={arrow} className="arrow"/>
+      </div>
+    </div>
+    <div className="banner-box f0">
+      <img src={page2}/>
+    </div>
+    <div className="banner-box f0">
+      <img src={page3}/>
+      <a href="#form" onClick={toForm} className="free-btn free-btn2">
+        <img src={freeBtn}/>
+      </a>
+    </div>
+  </>
+}
+
+const App: React.FC<{}> = ({}) => {
+  const [isForm, setIsForm] = useState(false);
+  const toForm = React.useCallback(() => setIsForm(true), []);
+  return (
+    <Store.Provider value={{ isForm }}>
+      <div className="main">
+        {
+          !isForm
+            ? <Main toForm={toForm}/>
+            : <Suspense fallback={<div className="page-loading">Loading...</div>}>
+              <Apply isForm={isForm}/>
+            </Suspense>
+        }
+      </div>
+    </Store.Provider>
+  );
+};
 
 export default App;
